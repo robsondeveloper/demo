d46d96a510b5aa3317a05b5d9258b55dbf32a522
Robson Oliveira da Costa <robsondeveloper@gmail.com>
2020-06-23 15:21:40 -0400

    cors and server port config

--- src/main/java/com/example/demo/ProductController.java
+++ src/main/java/com/example/demo/ProductController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/products")
 public class ProductController {
 
+    @CrossOrigin
     @GetMapping
     public List<Product> all() {
         List<Product> products = new ArrayList<>();
